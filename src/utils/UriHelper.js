97049bd4043083c0918fb5c3eebe77a47a60c266
Jalen Jackson <jallenjack201@hotmail.com>
2020-02-18 01:49:58 -0500

    hardcode api key so no confusion

--- src/utils/UriHelper.js
+++ src/utils/UriHelper.js
@@ -1,3 +1,3 @@
 export const UriHelper = (method) => {
-   return `https://www.alphavantage.co/query?function=${method}&apikey=${process.env.REACT_APP_API_KEY}`;
+   return `https://www.alphavantage.co/query?function=${method}&apikey=RXG32M4MQSBNDZKB`;
 };
