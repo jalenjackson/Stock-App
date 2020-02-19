e8675a2e60d4b03fa84a951df328983e46c4583b
Jalen Jackson <jallenjack201@hotmail.com>
2020-02-18 19:00:36 -0500

    Updates

---
+++ precache-manifest.b93a54a909ac170fb6c313f93528f5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4ccbfc86c1f63bfe69dcc8669dffa1",
+    "url": "/qs-coding-challenge/index.html"
+  },
+  {
+    "revision": "8ea5ab0ac09d608dbf9f",
+    "url": "/qs-coding-challenge/static/css/main.dd2e24dd.chunk.css"
+  },
+  {
+    "revision": "356e22b866b68287235a",
+    "url": "/qs-coding-challenge/static/js/2.82e46640.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/qs-coding-challenge/static/js/2.82e46640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea5ab0ac09d608dbf9f",
+    "url": "/qs-coding-challenge/static/js/main.1739e5b1.chunk.js"
+  },
+  {
+    "revision": "1db5543e53b88dd3f729",
+    "url": "/qs-coding-challenge/static/js/runtime-main.50796b55.js"
+  }
+]);
